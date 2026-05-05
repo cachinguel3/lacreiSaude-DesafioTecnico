26d5ad0726f75ec7dfd5fa981f8e2ca0e67b17a2
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-05-05 15:28:17 -0300

    paginas temporarias

---
+++ src/app/(pages)/termos-de-uso/page.tsx
@@ -0,0 +1,8 @@
+export default function termosDeUso() {
+    return (
+        <div className="flex flex-1 flex-col items-center justify-center gap-6 p-6 text-center sm:p-16">
+            <h1 className="text-4xl font-bold text-emerald-900 sm:text-6xl">Termos de Uso</h1>
+            <p className="text-lg text-slate-700 sm:text-xl">Em breve, esta página estará disponível com informações sobre os termos de uso do nosso serviço.</p>
+        </div>
+    );
+}
